b8b28609813cffd7d8b03562287dc0bc9aaf225a
al415607 <114917263+al415607@users.noreply.github.com>
2024-12-09 04:00:13 +0100

    Modelos (entidades) implementados

---
+++ src/Entity/User.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue()]
+    #[ORM\Column(type:"integer")]
+    private $id;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private $name;
+
+    #[ORM\Column(type: "string", length: 255, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: "integer")]
+    private $age;
+
+    #[ORM\Column(type: "string", length: 255)]
+    private $password;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+}
